95f02680d9453feb1e603fa3673bf94b8cd759e5
George Eaton <george_eaton@hotmail.co.uk>
2016-11-15 13:12:41 +0000

    finished #6 - added maximum topup value

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -2,12 +2,21 @@ class Oystercard
 
 attr_reader :balance
 
+  MAXIMUM_BALANCE = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Card cannot be loaded over £#{MAXIMUM_BALANCE}." if maximum_balance?
     @balance += amount
+  end
+
+  private
+
+  def maximum_balance?
+    @balance >= MAXIMUM_BALANCE
   end
 
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -3,13 +3,22 @@ require 'oyster_card'
 describe Oystercard do
 subject(:oyster) { described_class.new }
 
-  it 'should have a balance' do
-    expect(oyster.balance).to eq 0
+    context "new card" do
+    it 'should have a default balance' do
+      expect(oyster.balance).to eq 0
+    end
   end
 
-  it 'should add money to the card' do
-    oyster.top_up(10)
-    expect(oyster.balance).to eq 10
+    context "topping up" do
+      it 'should add money to the card' do
+        oyster.top_up(10)
+        expect(oyster.balance).to eq 10
+      end
+      it 'should have a maximum balance of £90' do
+        oyster.top_up(90)
+        expect{ oyster.top_up(1) }.to raise_error "Card cannot be loaded over £#{Oystercard::MAXIMUM_BALANCE}."
+      end
+
   end
 
-end
+  end
