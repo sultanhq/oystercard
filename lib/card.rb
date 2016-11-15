6ae535082b12735c8a3685f3f3e4a9e5aef01678
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-11-15 12:41:44 +0000

    Raised error for maximum balance capacity

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,15 @@
 class Oystercard
 
 attr_reader :balance
+
+  Maximum_capacity = 90
+
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    raise "Cannot top up: balance capacity of #{Maximum_capacity} has been exceeded" if @balance + amount > Maximum_capacity
+    @balance += amount
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,4 +2,26 @@ require 'card.rb'
 
 describe Oystercard do
 
+  subject(:card) { Oystercard.new }
+
+  context "top up card" do
+
+  it "should test that oystercard class responds to top up method" do
+    expect(card).to respond_to(:top_up).with(1).argument
+  end
+
+  it "should test that balance will change due to amount added" do
+    expect(card.top_up(10)).to eq 10
+  end
+end
+
+  context "raise error" do
+
+  it "should raise error if balance is more than 90" do
+    maximum_top_up = Oystercard::Maximum_capacity
+    subject.top_up(maximum_top_up)
+    expect{card.top_up(100)}.to raise_error("Cannot top up: balance capacity of #{maximum_top_up} has been exceeded")
+  end
+end
+
 end
