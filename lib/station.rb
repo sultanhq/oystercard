528ea2c5f4c7fac2bc65cbfe81b332e692228f4e
Dan Pelensky <dan@pelensky.com>
2016-11-17 11:27:46 +0000

    added station class, with name and zone

---
+++ lib/station.rb
@@ -0,0 +1,10 @@
+class Station
+
+  attr_reader :name, :zone
+
+  def initialize(name = "Aldgate", zone = 1)
+    @name = name
+    @zone = zone
+  end
+
+end
---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require "./lib/station"
+
+describe Station do
+subject(:station) {described_class.new}
+
+  it "should initialize with a name" do
+    expect(station.name).to eq "Aldgate"
+end
+
+it "should initialize with a zone" do
+  expect(station.zone).to eq 1
+end
+
+end
