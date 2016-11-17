f2a69d2943609d09d96a64671595f78155f028b6
George Eaton <george_eaton@hotmail.co.uk>
2016-11-17 11:33:34 +0000

    refactored to use self instead of instance variables

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -12,30 +12,31 @@ attr_reader :balance, :entry_station, :station_history
   end
 
   def top_up(amount)
-    fail "Card cannot be loaded over £#{MAXIMUM_BALANCE}." if @balance + amount > MAXIMUM_BALANCE
-    @balance += amount
+    fail "Card cannot be loaded over £#{MAXIMUM_BALANCE}." if self.balance + amount > MAXIMUM_BALANCE
+    self.balance += amount
   end
 
 
   def touch_in(entry_station)
-    fail "Not enough funds to travel" if @balance < MINIMUM_BALANCE
-    @entry_station = entry_station
+    fail "Not enough funds to travel" if self.balance < MINIMUM_BALANCE
+    self.entry_station = entry_station
   end
 
   def in_journey?
-    !@entry_station.nil?
+    !self.entry_station.nil?
   end
 
   def touch_out(exit_station)
     deduct(MINIMUM_BALANCE)
-    @station_history << {entry_station: @entry_station, exit_station: exit_station}
-    @entry_station = nil
+    self.station_history << {entry_station: self.entry_station, exit_station: exit_station}
+    self.entry_station = nil
   end
 
   private
+  attr_writer :balance, :entry_station
 
   def deduct(amount)
-    @balance -= amount
+    self.balance -= amount
   end
 
 end
